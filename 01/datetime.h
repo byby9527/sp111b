06444302f388346f8621d2c64c335ba39f3baddb
byby9527 <99935115+byby9527@users.noreply.github.com>
2023-06-13 15:35:28 +0800

    Create datetime.h

---
+++ 01/datetime.h
@@ -0,0 +1,9 @@
+#ifndef DATETIME_H_INCLUDED
+#define DATETIME_H_INCLUDED
+
+#include <time.h>
+
+void printCurrentTime(); // 顯示當前時間的函數宣告
+void printFormattedTime(const char* format);  // 顯示格式化時間的函數宣告
+
+#endif

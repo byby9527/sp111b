462292bc1c14016efca21f650bcbd87d42eba3f8
byby9527 <99935115+byby9527@users.noreply.github.com>
2023-06-13 18:49:39 +0800

    Update datetime.h

--- 01/datetime.h
+++ 01/datetime.h
@@ -1,9 +1,9 @@
 #ifndef DATETIME_H_INCLUDED
 #define DATETIME_H_INCLUDED
 
-#include <time.h>
+#include <stdio.h>
 
-void printCurrentTime(); // 顯示當前時間的函數宣告
-void printFormattedTime(const char* format);  // 顯示格式化時間的函數宣告
+void printCurrentTime();// 顯示當前時間的函數宣告
+void printFormattedTime(const char* format);// 顯示格式化時間的函數宣告
 
 #endif
